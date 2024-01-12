161ed390d95c1b7f850e08c3f8eb8d787fc03181
Timofey <timofey.sharipov@gmail.com>
2024-01-12 16:18:49 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/ArrayList/ArrayListApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.ArrayList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrayListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrayListApplication.class, args);
+	}
+
+}
